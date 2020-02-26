a08382075a944fc7e5f8c1e58fec1ec66c5d1229
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-26 07:56:11 -0500

    Create Array insert.js

---
+++ Array insert.js
@@ -0,0 +1,12 @@
+Array.prototype.insert = function(el, index) {
+  return this.splice(index, 0, el);
+}
+
+const arr = [
+  'Hello', 
+  'Hi'
+];
+arr.insert('Hey', 1);
+console.log(arr);
+/* Expected output:
+['Hello', 'Hey', 'Hi'] */

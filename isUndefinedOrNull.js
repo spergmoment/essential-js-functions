65caf07fea74f3625f2d9a0c831d499731b32af9
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-26 10:02:56 -0500

    Create isUndefinedOrNull.js

---
+++ isUndefinedOrNull.js
@@ -0,0 +1,21 @@
+function isUndefinedOrNull(obj) {
+  if(obj===undefined||obj===null||obj===""||obj===0||obj===NaN) return true;
+  return false;
+}
+console.log(isUndefinedOrNull(2))
+console.log(isUndefinedOrNull("h"))
+console.log(isUndefinedOrNull())
+console.log(isUndefinedOrNull(0))
+console.log(isUndefinedOrNull(""))
+console.log(isUndefinedOrNull(undefined))
+console.log(isUndefinedOrNull(null))
+console.log(isUndefinedOrNull("hey" - 89))
+/* Expected output:
+false
+false
+true
+true
+true
+true
+true
+false */

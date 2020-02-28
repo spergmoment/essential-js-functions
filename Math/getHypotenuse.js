aed80f92cdc0631dd082fc5cf2d6c8e62e783d7c
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-28 07:21:35 -0500

    py. triple

--- Math/getHypotenuse.js
+++ Math/getHypotenuse.js
@@ -1,6 +1,5 @@
 function getHypotenuse(x, y) {
   return Math.sqrt((x * x) + (y * y));
 }
-console.log(getHypotenuse(4, 13));
-/* Expected output:
-13.601470508735444 */
+console.log(getHypotenuse(3, 4));
+/* Expected output: 5 */

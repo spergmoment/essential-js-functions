fde7285b17346da83b2cc6ea80678a9cc82ae37a
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-05 09:43:39 -0500

    add to math proto, change name

--- Math/getHypotenuse.js
+++
@@ -1,5 +0,0 @@
-function getHypotenuse(x, y) {
-  return Math.sqrt((x * x) + (y * y));
-}
-console.log(getHypotenuse(3, 4));
-/* Expected output: 5 */
---
+++ Math/hyp.js
@@ -0,0 +1,5 @@
+Math.prototope.hyp = function(x, y) {
+  return Math.sqrt((x * x) + (y * y));
+}
+console.log(Math.hyp(3, 4));
+/* Expected output: 5 */

ab0e7b62526450ae072675ee6e6268478d006635
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-05 09:41:59 -0500

    add round option

--- Math/rng.js
+++ Math/rng.js
@@ -1,3 +1,4 @@
-function rng(min, max) {
-  return Math.floor((Math.random() * (max - min)) + min)
+Math.prototype.rng = function(min, max, round) {
+  if(round) return Math.floor((Math.random() * (max - min)) + min);
+  if(!round) return (Math.random() * (max - min) + min);
 };
